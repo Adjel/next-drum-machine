fb32ace9aab9a61daa1951322609287d61d82537
Adjel <adjel.touaoula@gmail.com>
2024-04-04 13:04:38 +0200

    exercise

--- src/components/DrumMachine/DrumMachine.js
+++ src/components/DrumMachine/DrumMachine.js
@@ -1,12 +1,15 @@
-'use client';
-import React from 'react';
-import useSound from 'use-sound';
+"use client";
+import React from "react";
+import useSound from "use-sound";
 
-import styles from './DrumMachine.module.css';
+import styles from "./DrumMachine.module.css";
+import { HandleSoundContext } from "../Header";
 
-const SOUND_SRC = '/909-drums.mp3';
+const SOUND_SRC = "/909-drums.mp3";
 
 function DrumMachine() {
+  const soundToggle = React.useContext(HandleSoundContext);
+
   const [play] = useSound(SOUND_SRC, {
     sprite: {
       kick: [0, 350],
@@ -14,31 +17,17 @@ function DrumMachine() {
       snare: [666, 290],
       cowbell: [968, 200],
     },
-    soundEnabled: true,
+    soundEnabled: soundToggle,
   });
 
   return (
     <div className={styles.wrapper}>
-      <button
-        onClick={() => play({ id: 'kick' })}
-      >
+      <button onClick={() => play({ id: "kick", volume: soundEnabled })}>
         Kick
       </button>
-      <button
-        onClick={() => play({ id: 'hihat' })}
-      >
-        Hat
-      </button>
-      <button
-        onClick={() => play({ id: 'snare' })}
-      >
-        Snare
-      </button>
-      <button
-        onClick={() => play({ id: 'cowbell' })}
-      >
-        Cowbell
-      </button>
+      <button onClick={() => play({ id: "hihat" })}>Hat</button>
+      <button onClick={() => play({ id: "snare" })}>Snare</button>
+      <button onClick={() => play({ id: "cowbell" })}>Cowbell</button>
     </div>
   );
 }
